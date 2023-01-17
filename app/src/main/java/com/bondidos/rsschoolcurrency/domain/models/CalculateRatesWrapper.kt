1a4bb2304621d8336ff8e9f4c4cfa4009c495eb4
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 22:59:01 +0300

    Update CalculateRatesWrapper.kt
    Для точных вычислений лучше использовать BigDecimal

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/models/CalculateRatesWrapper.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/models/CalculateRatesWrapper.kt
@@ -1,6 +1,6 @@
 package com.bondidos.rsschoolcurrency.domain.models
 
 data class CalculateRatesWrapper(
-    val rate: Double,
+    val rate: Double, // Для точных вычислений лучше использовать BigDecimal
     val list: List<CurrencyUi>,
 )
