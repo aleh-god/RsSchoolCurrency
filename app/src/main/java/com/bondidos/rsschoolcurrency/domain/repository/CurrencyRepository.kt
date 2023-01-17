c60973bfef4376b55d52859ef88b1f0b2e7a0bf6
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 23:05:27 +0300

    Update CurrencyRepository.kt
    пакет нужно назвать не репозиторий

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/repository/CurrencyRepository.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/repository/CurrencyRepository.kt
@@ -1,3 +1,4 @@
+// Возможно, пакет нужно назвать не репозиторий. Так как с помощью интерфейсов можно обращаться не только к нему в ДАТА слое
 package com.bondidos.rsschoolcurrency.domain.repository
 
 import com.bondidos.rsschoolcurrency.domain.models.Currency
