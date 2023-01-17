566ea0f6c936d2a0f4e28e953851bf729c6a10f4
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 23:02:34 +0300

    Update SortListWrapper.kt
    оборачивать нужно три и больше параметра

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/models/SortListWrapper.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/models/SortListWrapper.kt
@@ -1,5 +1,6 @@
 package com.bondidos.rsschoolcurrency.domain.models
 
+// Все же для удобства чтения кода, оборачивать нужно три и больше параметра
 data class SortListWrapper(
     val item: CurrencyUi,
     val list: List<CurrencyUi>,
