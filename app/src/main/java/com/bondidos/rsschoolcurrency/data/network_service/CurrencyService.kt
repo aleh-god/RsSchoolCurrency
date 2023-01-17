5ea56e18ea1a752c71b992497c4bab2bf192a330
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 22:24:07 +0300

    Update CurrencyService.kt
        // TODO("часть строки можно убрать в BASE URL")

--- app/src/main/java/com/bondidos/rsschoolcurrency/data/network_service/CurrencyService.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/data/network_service/CurrencyService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 
 interface CurrencyService {
 
+        // TODO("часть строки можно убрать в BASE URL")
     @GET("/myWebsiteBackend/api/currency/")
     suspend fun fetchAll(): List<CurrencyApi>
 }
