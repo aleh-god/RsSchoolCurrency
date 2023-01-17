041bbbae5c98efccc6dbe92e31c7490c0aed7222
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 23:18:22 +0300

    Update FetchAllCurrencyesUseCase.kt
    Пустой юз-кейс с пробросом метода из репозитория

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/use_cases/FetchAllCurrencyesUseCase.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/use_cases/FetchAllCurrencyesUseCase.kt
@@ -8,5 +8,6 @@ import javax.inject.Inject
 class FetchAllCurrenciesUseCase @Inject constructor(
     private val repository: CurrencyRepository
 ) : OutUseCase<Flow<List<Currency>>> {
+    // Пустой юз-кейс с пробросом метода из репозитория. Такое было модно в 2020 году. В 21 критиковали сеньоры. В 22 напишу уже и я. Должен быть отдельный прямой интерфейс к репе.
     override fun invoke(): Flow<List<Currency>> = repository.fetchAllCurrencies()
 }
