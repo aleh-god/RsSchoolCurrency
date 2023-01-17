1b0973fc373305a35750af836697c4bc801cbc69
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 22:51:08 +0300

    Update BaseMapper.kt
    Для чего нужен интерфейс?

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/mapers/BaseMapper.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/mapers/BaseMapper.kt
@@ -1,5 +1,7 @@
 package com.bondidos.rsschoolcurrency.domain.mapers
 
+// Для чего нужен интерфейс? Ты не используешь его в итераторах, не принимаешь в конструкторы.
+// Если надо было выстроить иерархию мапперов, тогда для этого используется sealed interface
 interface BaseMapper<In,Out> {
     operator fun invoke(params: In) : Out
 }
