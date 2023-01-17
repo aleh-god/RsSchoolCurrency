d666faa001216a43740aa8a352ecf14f2ada46f8
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 22:49:01 +0300

    Update MoveToTheTopMapper.kt
    MutableList

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/mapers/MoveToTheTopMapper.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/mapers/MoveToTheTopMapper.kt
@@ -8,8 +8,11 @@ class MoveToTheTopMapper : BaseMapper<SortListWrapper, List<CurrencyUi>> {
         val clickedItem: CurrencyUi? = params.list.find {
             it.baseName == params.item.baseName
         }
+        // Наверное, цепочку можно было продолжить без val
         clickedItem?.let {
+            // Возвращаешь мутабельный лист, пусть и под интерфейсом листа. 
             return params.list.toMutableList().apply {
+                // Можно было сделать через .map
                 remove(clickedItem)
                 add(index = 0, clickedItem.copy(rate = ""))
             }
