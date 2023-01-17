0019e901ec3fba1f3c510523a7deb0ce8bd03ea5
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 23:12:36 +0300

    Update CalculateMultiplierUseCase.kt
    Сайд эффект в домейн слое

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/use_cases/CalculateMultiplierUseCase.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/use_cases/CalculateMultiplierUseCase.kt
@@ -7,7 +7,9 @@ class CalculateMultiplierUseCase : InOutUseCase<CalculateMultiplierWrapper, Doub
         val itemInFocus = params.latestList.find {
             it.baseName == params.basename
         }
+        // Сайд эффект в домейн слое или плохой нейминг метода
         itemInFocus ?: throw java.lang.IllegalArgumentException()
+        // Эти методы могут выбросить исключение. Возможно, где-то они отлавливаются, но по архитектуре это неправильно.
         val newCurrencyValue = params.text.toString().toDouble()
         val previousCurrencyValue = itemInFocus.rate.toDouble()
         return newCurrencyValue/previousCurrencyValue
