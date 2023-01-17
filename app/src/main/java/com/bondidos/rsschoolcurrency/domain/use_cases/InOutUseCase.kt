b430cd852094ede273bc916a28688ec746a6096c
Bogush Alexandr <71838670+aleh-god@users.noreply.github.com>
2023-01-17 23:20:39 +0300

    Update InOutUseCase.kt
    Ненужные интерфейсы.

--- app/src/main/java/com/bondidos/rsschoolcurrency/domain/use_cases/InOutUseCase.kt
+++ app/src/main/java/com/bondidos/rsschoolcurrency/domain/use_cases/InOutUseCase.kt
@@ -1,5 +1,8 @@
 package com.bondidos.rsschoolcurrency.domain.use_cases
 
+// Тоже замечание, что и к интерфесу маппера. 
+// Не используешь в инверсии контроля, не используешь в тестах.
+// Если нужно для иерархии классов - то sealed interface
 interface InOutUseCase<In, Out> {
     operator fun invoke(name: In): Out
 }
